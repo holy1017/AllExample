7e836534b026d7d18fbe985f73acf290667b7782
lilly_000 <lilly_000@lilly>
2015-12-02 17:46:15 +0900

    초기 업로드

---
+++ src/main/java/com/mkyong/servlet3/MyWebInitializer.java
@@ -0,0 +1,25 @@
+package com.mkyong.servlet3;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.mkyong.config.SpringWebConfig;
+
+public class MyWebInitializer extends
+		AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { SpringWebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/spring4-mvc-ajax-example/*" };
+	}
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+}
